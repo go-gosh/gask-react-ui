49dcfc3e94a5ce09b1b4f73b6c2e73ac4308f904
CyrivlClth <cyrivlclth@live.com>
2023-02-13 00:00:08 +0800

    feat: Task detail base page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+    },
+  },
+  plugins: [],
+}
